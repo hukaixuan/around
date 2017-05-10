7b2fde51b61095f6d886ea6c43cb4579be7564c3
hukx.michael <1782964880@qq.com>
2017-05-10 20:24:48 +0800

    修数据库连接

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -4,3 +4,9 @@ from flask import Blueprint
 auth = Blueprint('auth', __name__)
 
 from . import views
+
+from .. import db
+
+@auth.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,8 +4,13 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors, util
+from .. import db
 
 # 将此函数注入到各个template中
 @main.context_processor
 def inject_func():
 	return dict(get_distance=util.get_distance)	# 根据经纬度信息计算两点间距离
+
+@main.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
